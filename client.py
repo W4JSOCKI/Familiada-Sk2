800f8cbc4ddf2b20b8ca3e98300e370a8fdf8fd6
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2022-12-06 10:49:15 +0100

    komentarz do inputu i przykład gui

---
+++ client.py
@@ -0,0 +1,15 @@
+import PySimpleGUI as sg
+
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+window = sg.Window('Window Title', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
