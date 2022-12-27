9d8cc61c7924bf7c6e2c05e0c03b186fa6faba1c
W4JSOCKI <105747490+W4JSOCKI@users.noreply.github.com>
2022-12-27 22:04:45 +0100

    Loading database from file
    not working correctly yet

--- server_1.0.cpp
+++ server_1.0.cpp
@@ -7,6 +7,8 @@
 #include <time.h>
 #include <queue>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 
 using namespace std;
@@ -15,24 +17,25 @@ class Player{
     int id;
     string nickname;
     string ip;
-    Player(int newid){
+
+  public:  Player(int newid){
         id=newid;
         nickname="notset";
         ip="notset";
     }
     
-    public int setnickname(string newnickname) {
-        if(newnickname.lenght()<3)
+    public: int setnickname(string newnickname) {
+        if(newnickname.length()<3)
             return -1;
         else nickname=newnickname;
         return 0;
     }
-    public string getnickname(){return nickname};
-    public int getid(){return id};
-    public string getip(){return ip};
+    public: string getnickname(){return nickname;}
+    public:  int getid(){return id;}
+    public: string getip(){return ip;}
     
     
-}
+};
 
 class Game {
     int id;
@@ -40,10 +43,10 @@ class Game {
     vector <Player> players;//0-2 team1 3-5 team 2
 
     short round;// actual round number 0 if game not started
-
+    string nicknames[6];
     string question;
     string responses[5];
-    short points[5]// points per response
+    short points[5];// points per response
 
     short team1points,team2points;
     short team1fails,team2fails;
@@ -51,7 +54,7 @@ class Game {
     short point_multiplier;// first 3 rounds = 1 then goes one up every round;
     short point_treshhold =300; // points needed to win the game
     
-    Game(int newid){
+  public:  Game(int newid){
         id=newid;
         how_many_players=0;
         for(int i=0; i<6;i++)
@@ -65,28 +68,67 @@ class Game {
         buttonwin=0;
          
     }
+    public:
+    string questions [1001];
+    string answers [1001][6];
+    int pointvalue [1001][6];
+   
+    void loaddata(){ // not sure if it should be in Game class
+       string line;
+       ifstream baza;
+       baza.open("Baza.csv");
+       for(int i=1; i<1001; i++){
+        getline(baza,line);
+        cout << line << endl;
+        stringstream linestream(line);
+        getline(linestream,questions[i],';');
+        for(int j=1; j<6; j++){
+            getline(linestream,answers[i][j],';');
+            string p;
+            getline(linestream,p,';');
+           // pointvalue[i][j]=stoi(p);
+        }
+       }
+      // baza.close();
+
+    }
     
-    public int addplayer(Player NewPlayer{
+    public: int addplayer(Player NewPlayer){
         if(how_many_players < 6){
-        players.pushback(NewPlayer);
+        players.push_back(NewPlayer);
         how_many_players++;
         return how_many_players;}  
         else
             return -1;
     }
     
-    public int removeplayer(int id){
+     int removeplayer(int id){
         for(int i =0; i<6;i++){
-            if(players[i].getid==id){
-                players.erase(i);
+            if(players[i].getid()==id){
+                players.erase(players.begin()+i);
                 how_many_players--;
                 return how_many_players;
             } 
         }
         return -1;
-        
     }
-                          
-                              
-
+                                                     
+};
+int main()
+{
+    Game test(1);
+    Player player1(1);
+    Player player2(2);
+    player1.setnickname("Hutnor");
+    player2.setnickname("szbanek");
+    cout << test.addplayer(player1) << endl;
+    cout << test.addplayer(player2) << endl;
+    test.loaddata();
+    for(int i=1; i<50; i++)
+    {
+        cout << test.questions[i] << endl;
+       // for(int j=1; j<6; j++)
+   //     cout << test.answers[i][j] << " " << endl;// test.pointvalue[i][j] << endl;
+    }
+    return 0;
 }
