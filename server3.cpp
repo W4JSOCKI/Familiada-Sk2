2f060bf216c2e4e284a520440076035a4a3d24c1
W4JSOCKI <105747490+W4JSOCKI@users.noreply.github.com>
2023-02-15 20:13:25 +0100

    Update server3.cpp

--- server3.cpp
+++ server3.cpp
@@ -18,6 +18,10 @@ using namespace std;
 int in;
 FILE *fp;
 
+struct timeval timeout;
+timeout.tv_sec = 120;
+timeout.tv_usec = 0;
+
 void save_to_file(int client_fd) {
 
 	
@@ -57,14 +61,13 @@ cout << "weszł" << endl;
     for (int client_fd : clients) {
         int sent_bytes = 0;
         i++;
-        std::string file_buffer_id= std::to_string(i)+"\n"+file_buffer;
+        std::string file_buffer_id= std::to_string(clientids[i])+"\n"+file_buffer;
         char*file_buffer_idc=(char*)file_buffer_id.c_str();
         file_size=file_buffer_id.size();
         while (sent_bytes < file_size) {
             sent_bytes += send(client_fd, file_buffer_idc, file_size, 0);
             cout << file_buffer << endl;
-         
-        
+
         }
     }
     file.close();
@@ -89,14 +92,14 @@ int main()
     struct epoll_event event;
     struct epoll_event *events;
 
-    // Create server socket
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         std::cerr << "Failed to create server socket" << std::endl;
         return 1;
     }
 
-    // Set socket options
+
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         std::cerr << "Failed to set socket options" << std::endl;
@@ -107,21 +110,21 @@ int main()
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind socket to address
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         std::cerr << "Failed to bind socket to address" << std::endl;
         return 1;
     }
 
-    // Listen for connections
+
     if (listen(server_fd, MAX_CLIENTS) < 0)
     {
         std::cerr << "Failed to listen for connections" << std::endl;
         return 1;
     }
 
-    // Create epoll file descriptor
+
     if ((epoll_fd = epoll_create1(0)) < 0)
     {
         std::cerr << "Failed to create epoll file descriptor" << std::endl;
@@ -140,20 +143,21 @@ int main()
 
     while (true)
     {	
-        int n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        int n = epoll_wait(epoll_fd, events, MAX_EVENTS, timeout);
         for (int i = 0; i < n; i++)
         {		cout << "XD"<< endl;
-            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) || (!(events[i].events & EPOLLIN)))
+            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) )//(!(events[i].events & EPOLLIN)))
             {
             
                 std::cerr << "Error on epoll wait" << std::endl;
                 close(events[i].data.fd);
                 continue;
             }
-            else if (server_fd == events[i].data.fd)
+            else if (server_fd == events[i].data.fd && clients.size() < MAX_CLIENTS)
+            {
             {	
             cout << "2" << endl;
-                // New connection
+             
                 while (clients.size() < MAX_CLIENTS && (client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0)
                 {
                     std::cout << "Assigned unique ID " << clients.size() + 1 << " to new client" << std::endl;
@@ -189,7 +193,7 @@ int main()
             }
             else
             {	cout << "3" << endl;
-                // Data from client
+             
                 int done = 0;
                 int client_index = -1;
                 for (int j = 0; j < clients.size(); j++)
@@ -200,15 +204,18 @@ int main()
                         break;
                     }
                 }
+
                 if (client_index < 0)
                 {
-                    std::cerr << "Error finding client index" << std::endl;
-                    return 1;
+                   send_file_to_clients(clients,"out.txt");
+                }else
+                {
+                    save_to_file(events[i].data.fd);
+    		        sleep(3);
+                    send_file_to_clients(clients,"out.txt");
                 }
                 
-                save_to_file(events[i].data.fd);
-    		sleep(3);
-                send_file_to_clients(clients,"out.txt");
+                
                 if (done)
                 {
                     std::cout << "Closing connection with client " << client_index + 1 << std::endl;
